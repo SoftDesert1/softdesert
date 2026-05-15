2ebacc4c53ade7b295c66d2e61d189608fc76b30
Jean Silva <jeanvitor.nnsilva@hotmail.com>
2026-05-15 17:53:20 -0300

    xxx

--- next.config.ts
+++ next.config.ts
@@ -1,22 +1,35 @@
-import type { NextConfig } from "next";
+import type {
+  NextConfig,
+} from "next";
 
 const nextConfig: NextConfig = {
+
   images: {
+
     remotePatterns: [
 
       {
         protocol: "https",
-        hostname: "images.unsplash.com",
+        hostname:
+          "images.unsplash.com",
       },
 
       {
         protocol: "https",
-        hostname: "s1.pearlcdn.com",
+        hostname:
+          "s1.pearlcdn.com",
       },
 
       {
         protocol: "https",
-        hostname: "iofslrvqmnpxymszbdby.supabase.co",
+        hostname:
+          "iofslrvqmnpxymszbdby.supabase.co",
+      },
+
+      {
+        protocol: "https",
+        hostname:
+          "cdn.discordapp.com",
       },
 
     ],
